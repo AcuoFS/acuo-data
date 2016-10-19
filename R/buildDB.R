eda9ed5def46562931b7a365cdf52c6833972b32
Lucie Le Rolland <lucie.le.rolland@gmail.com>
2016-10-19 16:12:33 +0800

    OW-47 Added zcs.load in buildDB.R

--- R/buildDB.R
+++ R/buildDB.R
@@ -24,6 +24,7 @@ buildDataBase = function() {
                  'optionsvanilla.load', 
                  'optionsbarrier.load', 
                  'fra.load',
+                 'zcs.load', 
                  'assetCategory.load',
                  'assetInventory.load')
   
