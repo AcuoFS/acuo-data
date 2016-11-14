18c5d90742c38f9b0453023867f385e189748852
Lucie Le Rolland <lucie.le.rolland@gmail.com>
2016-11-14 10:30:30 +0800

    Changed folder and file names so that current tests are now identified as DB tests

--- R/testDB.R
+++ R/testDB.R
@@ -3,7 +3,7 @@ library('RUnit')
 source('R/buildDB.R')
 
 test.suite = defineTestSuite("example",
-                dirs = file.path("R/test"),
+                dirs = file.path("R/testDB"),
                 testFileRegexp = 'R')
 
 test.result <- runTestSuite(test.suite)
