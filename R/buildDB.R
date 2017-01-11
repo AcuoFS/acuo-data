c4c3072a08de7d58774b13385125fd153bebb759
lucielerolland <lucie.le.rolland@gmail.com>
2017-01-11 13:32:51 +0800

    OW-73

--- R/buildDB.R
+++ R/buildDB.R
@@ -11,33 +11,33 @@ buildDataBase = function() {
   
   clear(graph,input=FALSE)
   
-  load.name <- c('cypher/firms.load',
-                 'cypher/legalentities.load',
-                 'cypher/clearingHouses.load',
-                 'cypher/fcms.load',
-                 'cypher/tradingAccounts.load',
-                 'cypher/bilateralAgreements.load',
-                 'cypher/clearedAgreements.load',
-                 'cypher/assetCategories.load',
-                 'cypher/assetInventory.load', 
-                 'cypher/custodians.load',
-                 'cypher/custodianAccounts.load',
-                 'cypher/counterpartCustodianAccounts.load',
-                 'cypher/custodianAssets.load',
-                 'cypher/mstatements.load',
-                 'cypher/initmcexp.load',
-                 'cypher/initmc.load',
-                 'cypher/mstatementspres.load',
-                 'cypher/initmcexppres.load',
-                 'cypher/initmcpres.load',
-                 'cypher/settings.load',
-                 'cypher/assetTransfer.load')
+  load.name <- c('/cypher/firms.load',
+                 '/cypher/legalentities.load',
+                 '/cypher/clearingHouses.load',
+                 '/cypher/fcms.load',
+                 '/cypher/tradingAccounts.load',
+                 '/cypher/bilateralAgreements.load',
+                 '/cypher/clearedAgreements.load',
+                 '/cypher/assetCategories.load',
+                 '/cypher/assetInventory.load', 
+                 '/cypher/custodians.load',
+                 '/cypher/custodianAccounts.load',
+                 '/cypher/counterpartCustodianAccounts.load',
+                 '/cypher/custodianAssets.load',
+#                 '/cypher/mstatements.load',
+#                 '/cypher/initmcexp.load',
+#                 '/cypher/initmc.load',
+                 '/cypher/mstatementspres.load',
+                 '/cypher/initmcexppres.load',
+                 '/cypher/initmcpres.load',
+                 '/cypher/settings.load',
+                 '/cypher/assetTransfer.load')
   
   for (i in 2:6) {
       load.name <- c(load.name, paste('/cypher/info', toString(i), '.load', sep=''))
     }
   
-  load.preurl<-'https://raw.githubusercontent.com/AcuoFS/acuo-data/develop/graph-data/'
+  load.preurl<-'https://raw.githubusercontent.com/AcuoFS/acuo-data/develop/graph-data'
   
   load.url<-paste(load.preurl,sep='',load.name)
   
